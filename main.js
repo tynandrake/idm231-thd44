cdf31e972469f7b99561ae1841816134e5223e43
tynandrake <71619265+tynandrake@users.noreply.github.com>
2021-01-27 23:03:41 -0500

    added javascript - date arrays, zodiac calander

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const aquarius = document.getElementById('miseducation');
+const pisces = document.getElementById('blonde');
+const aries = document.getElementById('mbdtf');
+const taurus = document.getElementById('blueprint');
+const gemini = document.getElementById('telefone');
+const cancer = document.getElementById('take-care');
+const leo = document.getElementById('stankonia');
+const virgo = document.getElementById('swimming');
+const libra = document.getElementById('under-pressure');
+const scorpio = document.getElementById('monster');
+const sagittarius = document.getElementById('eternal-atake');
+const capricorn = document.getElementById('tpab');
+
+// Expand Card
 let buttons = document.querySelectorAll('button .box'); 
 
 buttons.forEach(button => {
@@ -7,6 +21,8 @@ buttons.forEach(button => {
     });
 });
 
+
+// Audio
 document.addEventListener('DOMContentLoaded', init);
     const SOUNDS = {};
     
@@ -62,3 +78,64 @@ document.addEventListener('DOMContentLoaded', init);
         audio.src = src;
         audio.play();
     };
+
+
+// Get Date
+let date = [];
+
+function getInput(evt) {
+    evt.preventDefault();
+    const $userInput = document.getElementById('userInput');
+    const userInput = $userInput.value;
+    const $userMonth = document.getElementById('month');
+    const userMonth = $userMonth.value;  
+
+    date.push(userMonth , userInput);
+    document.forms[0].reset();
+
+    console.log({date});
+};  
+
+const $form = document.querySelector('.input-group.mb-3 form');
+$form.addEventListener('submit', getInput);
+
+//Display Album
+
+// if ((date[0] == 'january' && date[1]>=20) || (date[0] == 'february' && date[1]<=18)) {
+//     document.getElementById('miseducation').innerHTML = ;
+// } else if ((date[0] == 'february' && date[1]>=19) || (date[0] == 'march' && date[1]<=20)) {
+//     pisces;
+// } else if ((date[0] == 'march' && date[1]>=21) || (date[0] == 'april' && date[1]<=19)) {
+//     aries;
+// } else if ((date[0] == 'april' && date[1]>=20) || (date[0] == 'may' && date[1]<=20)) {
+//     taurus;
+// } else if ((date[0] == 'may' && date[1]>=21) || (date[0] == 'june' && date[1]<=20)) {
+//     gemini;
+// } else if ((date[0] == 'june' && date[1]>=21) || (date[0] == 'july' && date[1]<=22)) {
+//     cancer;
+// } else if ((date[0] == 'july' && date[1]>=23) || (date[0] == 'august' && date[1]<=22)) {
+//     leo;
+// } else if ((date[0] == 'august' && date[1]>=23) || (date[0] == 'september' && date[1]<=22)) {
+//     virgo;
+// } else if ((date[0] == 'september' && date[1]>=23) || (date[0] == 'october' && date[1]<=22)) {
+//     libra;
+// } else if ((date[0] == 'october' && date[1]>=23) || (date[0] == 'november' && date[1]<=21)) {
+//     scorpio;
+// } else if ((date[0] == 'november' && date[1]>=22) || (date[0] == 'december' && date[1]<=21)) {
+//     sagittarius;
+// } else if ((date[0] == 'december' && date[1]>=22) || (date[0] == 'january' && date[1]<=19)) {
+//     capricorn;
+// };
+
+// aquarius.addEventListener('click' , function() {}); 
+// pisces = addEventListener('click' , function() {pisces});
+// aries = addEventListener('click' , function() {aries});
+// taurus = addEventListener('click' , function() {taurus});
+// gemini = addEventListener('click' , function() {gemini});
+// cancer = addEventListener('click' , function() {cancer});
+// leo = addEventListener('click' , function() {leo});
+// virgo = addEventListener('click' , function() {virgo});
+// libra = addEventListener('click' , function() {libra});
+// scorpio = addEventListener('click' , function() {scorpio});
+// sagittarius = addEventListener('click' , function() {sagittarius});
+// capricorn = addEventListener('click' , function() {capricorn});
